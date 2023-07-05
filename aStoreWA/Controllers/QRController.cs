a51f61157d36212a413e3e35471e2915c3c14477
zimkov <zimkov.leha@gmail.com>
2023-07-05 14:42:10 +0400

    qr code

---
+++ aStoreWA/Controllers/QRController.cs
@@ -0,0 +1,48 @@
+﻿using aStoreServer.Models;
+using IronBarCode;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace aStoreServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QRController : Controller
+    {
+        private readonly ApplicationContext _context;
+        public QRController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(nameof(id))]
+        [ActionName(nameof(GetQRById))]
+        public async Task<IActionResult> GetQRById([FromRoute] int id)
+        {
+            var entity = await _context.QR.FindAsync(id);
+            return Ok(entity);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<QR>> Post(QR value)
+        {
+            var entity = await _context.QR.AddAsync(value);
+            await _context.SaveChangesAsync();
+            var qrCode = QRCodeWriter.CreateQrCode(value.QrInfo, 300);
+            qrCode.AddBarcodeValueTextBelowBarcode();
+            qrCode.AddAnnotationTextAboveBarcode(entity.ToString());
+            qrCode.SaveAsPng(@"D:\tor\Qr.png");
+
+
+            return CreatedAtAction("GetQRById", new { id = entity.Entity.Id }, entity.Entity);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.QR.ToList());
+        }
+
+    }
+}
---
+++ aStoreWA/Models/QR.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace aStoreServer.Models
+{
+    public class QR
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("QrInfo")]
+        public string QrInfo { get; set; } = "";
+
+        [NotMapped]
+        [JsonIgnore]
+        [Column("Entity")]
+        public Entity? QREntity { get; set; }
+
+        [Column("Path")]
+        public string Path { get; set; } = "";
+    }
+}
